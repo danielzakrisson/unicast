2ab4c8606a2d31c69dbaca61b37d6ff6abaf5d6b
mattiasakesson <ma10s@hotmail.com>
2021-09-20 11:48:48 +0200

    add bestrange prediction

--- src/models/predict.py
+++ src/models/predict.py
@@ -1,11 +1,18 @@
-from src.models.expected_timefraction_predictions import expected_timefraction
+from src.models.expected_timefraction_predictions import expected_timefraction, best_range
 
 
 def model_predict(inp, model_data=[]):
 
-    lower_bound = inp.lower_bound
-    upper_bound = inp.upper_bound
-    time_horizon = inp.time_horizon
+    if inp.prediction_type == 'Expected_timefraction':
+        lower_bound = inp.lower_bound
+        upper_bound = inp.upper_bound
+        time_horizon = inp.time_horizon
+        res = expected_timefraction(lower_bound, upper_bound, time_horizon)
+        name = "Expected_timefraction"
+    else:
+        timefraction = inp.timefraction
+        time_horizon = inp.time_horizon
 
-    timefrac = expected_timefraction(lower_bound, upper_bound, time_horizon)
-    return {"Expected_timefraction": timefrac}
+        res = best_range(timefraction, time_horizon)
+        name = 'Best range'
+    return {name: res}
