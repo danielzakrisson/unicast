eefa34df7aba28a47817acf088b866de21a19de0
mattiasakesson <ma10s@hotmail.com>
2021-09-20 11:27:24 +0200

    small change in predict

--- src/models/predict.py
+++ src/models/predict.py
@@ -8,4 +8,4 @@ def model_predict(inp, model_data=[]):
     time_horizon = inp.time_horizon
 
     timefrac = expected_timefraction(lower_bound, upper_bound, time_horizon)
-    return {'Expected_timefraction': timefrac}
+    return {"Expected_timefraction": timefrac}
