d772a9b6392351e9caf26f48dba8fb35e17983a1
mattiasakesson <ma10s@hotmail.com>
2021-11-03 14:34:42 +0100

    updates

--- src/models/expected_timefraction_predictions.py
+++ src/models/expected_timefraction_predictions.py
@@ -1,7 +1,8 @@
 import numpy as np
-from src.models.import_ethereum_timeseries import ethereum_data
+from src.models.import_token_timeseries import token_data
 
-def time_in_zones(x, n):
+
+def time_in_zones_(x, n):
     nb = np.zeros((len(x) - n, n - 1))
     for i in range(0, len(x) - n):
         for j in range(1, n):
@@ -10,18 +11,22 @@ def time_in_zones(x, n):
     return nb
 
 
-def best_range(desired_timefraction=0.8, time_horizon=100):
+def expected_timefraction_(time_in_zones, lower_bound, upper_bound):
     
-    time, eth_value = ethereum_data()
-    r_data = eth_value[1:] / eth_value[:-1]
-    r = time_in_zones(r_data, time_horizon)
+    return np.mean(np.array([len(np.where((r_ < upper_bound) * (r_ > lower_bound))[0]) for r_ in time_in_zones])) / time_in_zones.shape[1]
+
+
+def best_range(desired_timefraction=0.8, time_horizon=100, **kwargs):
+
+    time, token_value = token_data(**kwargs)
+    r_data = token_value[1:] / token_value[:-1]
+    tiz = time_in_zones_(r_data, time_horizon)
     time_fraction = []
     bounds = []
     lower_bound = 0.0
     for upper_bound in np.arange(0.01, 5, 0.01):
         bounds += [upper_bound]
-        time_fraction += [
-            np.mean(np.array([len(np.where((r_ < upper_bound) * (r_ > lower_bound))[0]) for r_ in r])) / r.shape[1]]
+        time_fraction += [expected_timefraction_(tiz, lower_bound, upper_bound)]
         lower_bound = upper_bound
 
     best_delta = len(time_fraction)
@@ -38,3 +43,13 @@ def best_range(desired_timefraction=0.8, time_horizon=100):
             best_l_index = l_index
             best_delta = delta
     return {'lower_bound': bounds[best_l_index], 'upper_bound': bounds[best_l_index + best_delta]}
+
+
+def expected_timefraction(lower_bound, upper_bound, time_horizon, **kwargs):
+
+    time, token_value = token_data(**kwargs)
+    r_data = token_value[1:] / token_value[:-1]
+    tiz = time_in_zones_(r_data, time_horizon)
+    time_fraction = expected_timefraction_(tiz, lower_bound, upper_bound)
+    return {'time_fraction': time_fraction}
+
